9bc4302e05d2dc83483415753f1d8924dcda642c
Alek Shnayder <alek.shnayder@gmail.com>
2017-06-18 13:04:51 -0500

    Display story comments

--- public/assets/js/article.js
+++ public/assets/js/article.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 	$.get(window.location.origin + '/api/comment',function(reply){
 
+		reply.forEach(function(story) {
+
+			story.comments.forEach(function(comment){
+				$('#'+ story._id ).children('.comments').append('<p>' + comment + '</p>' );
+			});
+		});
 	});
 });
